b125640aa5cfcae8bd8e7ed309b3728cad17988d
Draginalo <tropicaltisa@gmail.com>
2025-01-19 16:42:54 -0500

    Set up testing environment for networking.

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Valve.Sockets.Library.Initialize();
+    private Button testButton;
 
+    private void Awake()
+    {
+        Debug.Log("Socket Lib Itialized");
+        Valve.Sockets.Library.Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnApplicationQuit()
     {
-        
+        Valve.Sockets.Library.Deinitialize();
+        Debug.Log("Quit and Socket Lib Deanitialized");
+    }
+
+    private void Start()
+    {
+        testButton = GetComponent<Button>();
+        if (testButton != null)
+        {
+            testButton.onClick.AddListener(RunServerSetUp);
+        }
+    }
+
+    private void RunServerSetUp()
+    {
+
     }
 }
