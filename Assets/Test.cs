4a7559dd3710ab012e9dc09cecb3408ce436f26f
Draginalo <tropicaltisa@gmail.com>
2025-01-20 12:26:57 -0500

    Started setting up server starting.

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
+using Valve.Sockets;
 
 public class Test : MonoBehaviour
 {
@@ -30,6 +33,59 @@ public class Test : MonoBehaviour
 
     private void RunServerSetUp()
     {
+        //Create socket for server
+        NetworkingSockets server = new NetworkingSockets();
 
+        //Gets server pollGroup
+        uint pollGroup = server.CreatePollGroup();
+
+        //Sets up some utils for monotoring the server
+        NetworkingUtils networkingUtils = new NetworkingUtils();
+
+        //Defined status callback for server monotering
+        StatusCallback statusCallback = (ref StatusInfo info) =>
+        {
+            switch (info.connectionInfo.state)
+            {
+                case ConnectionState.None:
+                    break;
+                case ConnectionState.Connecting:
+                    server.AcceptConnection(info.connection);
+                    server.SetConnectionPollGroup(pollGroup, info.connection);
+                    break;
+                case ConnectionState.Connected:
+                    Debug.Log("Client has been connected - ID: " + info.connection + " " + info.connectionInfo.address.GetIP());
+                    break;
+                case ConnectionState.ClosedByPeer:
+                //No break here I assume to log the disconnection error
+                case ConnectionState.ProblemDetectedLocally:
+                    server.CloseConnection(info.connection);
+                    Debug.Log("Client has been disconnected - ID: " + info.connection + " " + info.connectionInfo.address.GetIP())
+                    break;
+            }
+        };
+
+        //Sets server moderating callback
+        networkingUtils.SetStatusCallback(statusCallback);
+
+        //Defines the server address
+        Address address = new Address();
+
+        //Sets the server address to port 5000 
+        address.SetAddress("::0", 5000);
+
+        //Creates listening socket on server from set address
+        uint listenSocket = server.CreateListenSocket(ref address);
+
+        //Enable SPAN for this next part
+/*#if VALVESOCKET_SPAN
+        MessageCallback message = (in NetworkingMessage netMessage) => 
+        {
+            Debug.Log("Message received from - ID: " + netMessage.connection + ", Channel ID: " + netMessage.channel + ", Data length: " + netMessage.length);
+        };
+#else
+        const int maxMessages = 20;
+        NetworkingMessage[] netMessages = new NetworkingMessage[maxMessages];
+#endif*/
     }
 }
