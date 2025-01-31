58c56139e352c008fb48643373fe2601bb708595
gkaufman2026 <gerald.kaufman@mymail.champlain.edu>
2025-01-31 16:19:17 -0500

    Created Focus Time + Collider Visual

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,22 +1,61 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    [Header("Normal Player Controls")]
     public float movementSpeed;
+
+    [Header("Focus Time Controls")]
+    public float focusSpeed;
+    public KeyCode focusKey = KeyCode.LeftShift;
+
+    private GameObject circleChild;
+    private SpriteRenderer circleRenderer;
+
+    private bool isInFocusTime = false;
+    private float currentMoveSpeed;
 
     private Rigidbody2D rb;
     private Vector2 input;
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+        circleChild = FindFirstObjectByType<SpriteRenderer>().gameObject;
+        circleChild.transform.localScale = FloatToVec3(GetComponent<CircleCollider2D>().radius);
+        circleRenderer = circleChild.GetComponent<SpriteRenderer>();
     }
 
     void Update() {
+        // Handles the general movement
+        HandleInput();
+
+        // Handles Focus Time
+        HandleFocusTime();
+    }
+
+    private void HandleInput() {
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
 
         input.Normalize();
 
-        rb.velocity = input * movementSpeed;
+        currentMoveSpeed = isInFocusTime ? focusSpeed : movementSpeed;
+        rb.velocity = input * currentMoveSpeed;
     }
+
+    private void HandleFocusTime() {
+        if (Input.GetKeyDown(focusKey)) {
+            isInFocusTime |= !isInFocusTime;
+        }
+        if (Input.GetKeyUp(focusKey) && isInFocusTime) {
+            isInFocusTime &= !isInFocusTime;
+        }
+
+        circleRenderer.enabled = isInFocusTime;
+    }
+
+    public bool IsInFocusTime() { return isInFocusTime; }
+
+    private Vector3 FloatToVec3(float x) { return new Vector3(x, x, x); }
 }
