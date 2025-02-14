beb674116b629dcd600c01e437caa29b1766b220
tSchonfeldt-Aultman <tSchonfeldt-Aultman@203-3-24>
2025-02-14 16:38:47 -0500

    manual merge by ramy

---
+++ Assets/Scripts/Player/PlayerAttacks.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerAttacks : MonoBehaviour {
+    [Header("Keybinds")]
+    public KeyCode shootKey = KeyCode.Z;
+    public KeyCode bombKey = KeyCode.X;
+
+    [Header("Bullet Stats")]
+    [Range(1, 8)]
+    public float bulletSpeed = 4.5f;
+    public float bulletDelay = 0.1f;
+    public int bulletDamage = 1;
+
+    [Header("Homing Missle")]
+    public Transform target;
+    [Range(1, 8)]
+    public float rotateSpeed = 2f;
+    public bool isHomingBullet = false;
+
+    [Header("Other")]
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private GameObject screenObject;
+    [SerializeField] private Transform leftBulletSpawn, rightBulletSpawn;
+
+    private List<GameObject> bullets = new();
+    private bool isShooting = false/*, isHomingBullet = false*/;
+
+    private void Start() {
+        target = GameObject.FindGameObjectWithTag("Enemy").transform;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(shootKey) && !isShooting) {
+            StartCoroutine(ShootBullets());
+        }
+    }
+
+    void FixedUpdate() {
+        float screenYMin = screenObject.transform.position.y - screenObject.transform.localScale.y / 2;
+        float screenYMax = screenObject.transform.position.y + screenObject.transform.localScale.y / 2;
+
+        for (int i = bullets.Count - 1; i >= 0; i--) {
+            if (bullets[i] != null) {
+                bullets[i].transform.position += bulletSpeed * Time.deltaTime * Vector3.up;
+
+                if (isHomingBullet) {
+                    GameObject bullet = bullets[i];
+                    Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
+
+                    Vector2 direction = (Vector2) target.position - bulletRB.position;
+                    direction.Normalize();
+
+                    float rotation = Vector3.Cross(direction, bullet.transform.up).z;
+                    bulletRB.angularVelocity = -rotation * (rotateSpeed * 100);
+                    bulletRB.velocity = bullet.transform.up * bulletSpeed;
+                }
+
+                // Check if the bullet is out of the y range of the reference object
+                if (bullets[i].transform.position.y < screenYMin || bullets[i].transform.position.y > screenYMax) {
+                    Destroy(bullets[i]);
+                    bullets.RemoveAt(i);
+                }
+            }
+        }
+    }
+
+    private IEnumerator ShootBullets() {
+        isShooting = true;
+
+        while (Input.GetKey(shootKey)) {
+            GameObject leftBullet = Instantiate(bulletPrefab, leftBulletSpawn);
+            GameObject rightBullet = Instantiate(bulletPrefab, rightBulletSpawn);
+            bullets.Add(leftBullet);
+            bullets.Add(rightBullet);
+
+            yield return new WaitForSeconds(bulletDelay);
+        }
+        isShooting = false;
+    }
+
+    public int GetDamageAmount() { return bulletDamage; }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,67 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [Header("Normal Player Controls")]
+    public float movementSpeed;
+
+    [Header("Focus Time Controls")]
+    public float focusSpeed;
+    public KeyCode focusKey = KeyCode.LeftShift;
+
+    [Tooltip("I'm Jerry and I hate how it's styled")]
+    [SerializeField] private GameObject circleChild;
+
+    private SpriteRenderer circleRenderer;
+
+    private bool isInFocusTime = false;
+    private float currentMoveSpeed;
+
+    private Rigidbody2D rb;
+    private Vector2 input;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+
+        // Collider Visual - will automatically reset the visual to represent an accurate radius shape
+        circleChild.transform.localScale = FloatToVec3(GetComponent<CircleCollider2D>().radius);
+        circleRenderer = circleChild.GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        // Handles the general movement
+        HandleInput();
+
+        // Handles Focus Time
+        HandleFocusTime();
+    }
+
+    private void HandleInput() {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        input.Normalize();
+
+        // Switches between two movement
+        currentMoveSpeed = isInFocusTime ? focusSpeed : movementSpeed;
+        rb.velocity = input * currentMoveSpeed;
+    }
+
+    private void HandleFocusTime() {
+        // Handles the switch between if player is holding down the focus key and sets the isInFocusTime bool
+        if (Input.GetKeyDown(focusKey)) {
+            isInFocusTime |= !isInFocusTime;
+        }
+        if (Input.GetKeyUp(focusKey) && isInFocusTime) {
+            isInFocusTime &= !isInFocusTime;
+        }
+
+        // Enables the circle sprite depending if the player isInFocusTime
+        circleRenderer.enabled = isInFocusTime;
+    }
+
+    public bool IsInFocusTime() { return isInFocusTime; }
+
+    private Vector3 FloatToVec3(float x) { return new Vector3(x, x, x); }
+}
