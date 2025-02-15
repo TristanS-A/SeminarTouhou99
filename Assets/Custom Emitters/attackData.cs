9a356b0a6a1b23d00fce144e7824fd661bd0608c
William Mansfield <wMansfield@mtl.local>
2025-02-15 12:06:13 -0500

    started outlineing the sqeuencer by tracking of active emitters and spawning

--- Assets/Custom Emitters/attackData.cs
+++ Assets/Custom Emitters/attackData.cs
@@ -11,7 +11,21 @@ public class AttackData : ScriptableObject
 
     [SerializeField] Pattern attackPattern;
     [SerializeField] BaseBullet bulletType;
+    [SerializeField] GameObject emmiter;
 
     [SerializeField] float angle;
     
+    //getters dont really want code to change this object but we need to read from it
+    public bool IsCenterd()
+    {
+        return isCenterd; 
+    }
+    public Pattern GetPattern() { return attackPattern; }
+
+    public BaseBullet GetBullet() { return bulletType; }
+
+    public GameObject GetEmitter() { return emmiter; }
+    public float GetAngle() { return angle; }
+
+    public Vector2 GetDirction() { return dirction; }
 }
--- Assets/Custom Emitters/sequencer.cs
+++ Assets/Custom Emitters/sequencer.cs
@@ -2,9 +2,90 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Sequencer : MonoBehaviour
 {
     [SerializeField] List<AttackData> attacks;
+    [SerializeField] bool shouldLoop;
+    private int currentAttackIndex = 0;
+    private List<GameObject> activeEmmiter;
+
+    float timeTillNextAttack;
+    private void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+
+    }
     // Start is called before the first frame update
-  
+    private AttackData GetNextAttackEmmiter()
+    {
+        //we tried to move past the attacks that we have in the list
+        if (currentAttackIndex > attacks.Count)
+        {
+            //just retrun a null
+            if (shouldLoop)
+            {
+                currentAttackIndex = 0;
+            }
+            return null;
+
+        }
+
+        AttackData returnObj = attacks[currentAttackIndex];
+        currentAttackIndex++;
+
+        return returnObj;
+    }
+    private void SpawnEmmiter()
+    {
+        var nextAttackToSpawn = GetNextAttackEmmiter();
+
+        if (activeEmmiter == null)
+        {
+            //no new attacks to spawn
+            return;
+        }
+
+        Vector2 spawnPos = Vector2.zero;
+
+        //attack centerd on the enemy 
+        if (nextAttackToSpawn.IsCenterd())
+        {
+            spawnPos = this.transform.position;
+
+        }
+        else
+        {
+            spawnPos = (Vector2)this.transform.position + nextAttackToSpawn.GetDirction();
+        }
+
+        var obj = Instantiate(nextAttackToSpawn.GetEmitter(), spawnPos, Quaternion.identity);
+     
+        //before returning we need to clean the list for dead referecnes
+        CleanList();
+
+        return;
+    }
+    private void CleanList()
+    {
+        List<int> deadList = new List<int>();
+        int index = 0;
+
+        foreach (var emitter in activeEmmiter)
+        {
+            if(emitter.Equals(null))
+            {
+                deadList.Add(index);
+            }
+            index++;
+        }
+
+        foreach (var dead in deadList)
+        {
+            activeEmmiter.RemoveAt(dead);
+        }
+    }
 }
