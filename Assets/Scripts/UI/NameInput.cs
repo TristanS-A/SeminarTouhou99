5a63e9116bbec02e8ec2d649183e090b6332a8b6
Draginalo <tropicaltisa@gmail.com>
2025-03-09 01:43:18 -0500

    Started username entering.

---
+++ Assets/Scripts/UI/NameInput.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TMP_InputField))]
+public class NameInput : MonoBehaviour
+{
+    [SerializeField] private GameObject mJoinOptions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TMP_InputField nameField = GetComponent<TMP_InputField>();
+        nameField.onSubmit.AddListener(submit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void submit(string name)
+    {
+        mJoinOptions.SetActive(true);
+        Destroy(gameObject);
+    }
+}
