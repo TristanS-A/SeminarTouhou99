15a0cbf4500bed3bb12902ba8f0e8364fd0c8100
gkaufman2026 <gerald.kaufman@mymail.champlain.edu>
2025-03-28 17:24:30 -0400

    Implemented delay

--- Assets/Scripts/Player/PlayerAttacks.cs
+++ Assets/Scripts/Player/PlayerAttacks.cs
@@ -23,12 +23,17 @@ public class PlayerAttacks : MonoBehaviour {
 
     [Header("Defensive Bomb")]
     [SerializeField] private int maxDefensiveBombs = 3;
+    [SerializeField] private float defensiveBombDelay = 1;
     private int defensiveBombCount;
+    private bool isDefensiveBombDelayed = false;
 
     [Header("Offensive Bomb")]
     [SerializeField] private int maxOffensiveBombs = 3;
+    [SerializeField] private float offensiveBombDelay = 1;
     private int offensiveBombCount;
     private int attackIndex = 0;
+    private bool isOffensiveBombDelayed = false;
+
 
     private static int BOMB_COST = 1;
 
@@ -58,26 +63,22 @@ public class PlayerAttacks : MonoBehaviour {
             StartCoroutine(ShootBullets());
         }
 
-       
-        if(target == null)
-        {
+
+        if (target == null) {
             //this will need to be fixed
             var list = GameObject.FindGameObjectsWithTag("Enemy");
 
-            if(list == null)
-            {
+            if (list == null) {
                 return;
             }
 
             float closePosition = float.PositiveInfinity;
             Transform obj = null;
 
-            foreach (var item in list)
-            {
+            foreach (var item in list) {
                 Transform test = item.transform;
 
-                if (Vector2.Distance((Vector2)test.position, (Vector2)transform.position) < closePosition)
-                {
+                if (Vector2.Distance((Vector2)test.position, (Vector2)transform.position) < closePosition) {
                     obj = test;
                     closePosition = Vector2.Distance((Vector2)test.position, (Vector2)transform.position);
                 }
@@ -102,7 +103,7 @@ public class PlayerAttacks : MonoBehaviour {
                     GameObject bullet = bullets[i];
                     Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
 
-                    Vector2 direction = (Vector2) target.position - bulletRB.position;
+                    Vector2 direction = (Vector2)target.position - bulletRB.position;
                     direction.Normalize();
 
                     float rotation = Vector3.Cross(direction, bullet.transform.up).z;
@@ -120,22 +121,15 @@ public class PlayerAttacks : MonoBehaviour {
     }
 
     private void HandleDefensiveBomb(int cost) {
-        if (Input.GetKeyDown(defensiveBombKey) && defensiveBombCount > 0) {
-            defensiveBombCount -= cost;
-
-            var enemy = target.gameObject.GetComponent<TempEnemy>();
-            enemy.GetSequencer().CleanSequencer();
-
-            EventSystem.DefensiveBombAttack(defensiveBombCount);
+        if (!isDefensiveBombDelayed) {
+            StartCoroutine(DelayDefensiveBomb(defensiveBombDelay, cost));
         }
     }
 
     // Each bomb enables the sequencer on trigger and based on the lifetime of the attack will spawn and then disable, incrementing to the next index
     private void HandleOffensiveBomb(int cost) {
-        if (Input.GetKeyDown(offensiveBombKey) && offensiveBombCount > 0) {
-            offensiveBombCount -= cost;
-            EventSystem.OffensiveBombAttackUI(offensiveBombCount);
-            EventSystem.FireOffensiveBomb(transform.position);
+        if (!isOffensiveBombDelayed) {
+            StartCoroutine(DelayOffensiveBomb(offensiveBombDelay, cost));
         }
     }
 
@@ -169,17 +163,36 @@ public class PlayerAttacks : MonoBehaviour {
         attackIndex++;
     }
 
+    private IEnumerator DelayOffensiveBomb(float delay, int cost) {
+        isOffensiveBombDelayed = true;
+
+        if (Input.GetKeyDown(offensiveBombKey) && offensiveBombCount > 0) { 
+            offensiveBombCount -= cost;
+            EventSystem.OffensiveBombAttackUI(offensiveBombCount);
+            EventSystem.FireOffensiveBomb(transform.position);
+            yield return new WaitForSeconds(delay);
+        }
+        isOffensiveBombDelayed = false;
+    }
+
+    private IEnumerator DelayDefensiveBomb(float delay, int cost) {
+        isDefensiveBombDelayed = true;
+
+        if (Input.GetKeyDown(defensiveBombKey) && defensiveBombCount > 0) {
+            defensiveBombCount -= cost;
+            var enemy = target.gameObject.GetComponent<TempEnemy>();
+            enemy.GetSequencer().CleanSequencer();
+
+            EventSystem.DefensiveBombAttack(defensiveBombCount);
+            yield return new WaitForSeconds(delay);
+        }
+        isDefensiveBombDelayed = false;
+    }
+
     public int GetDefensiveBombCount => defensiveBombCount;
     public int GetOffensiveBombCount => offensiveBombCount;
-
     public static int GetBombCost => BOMB_COST;
+    private void OnEnable() => EventSystem.OnOffensiveBombAttack += SpawnOffensiveBomb;
+    private void OnDisable() => EventSystem.OnOffensiveBombAttack -= SpawnOffensiveBomb;
 
-    private void OnEnable() {
-        EventSystem.OnOffensiveBombAttack += SpawnOffensiveBomb;
-    }
-
-    // OFFLOADS UI FROM EVENT
-    private void OnDisable()  {
-        EventSystem.OnOffensiveBombAttack -= SpawnOffensiveBomb;
-    }
 }
