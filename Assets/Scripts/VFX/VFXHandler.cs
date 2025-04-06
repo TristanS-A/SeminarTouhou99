0a1558183d24614bd278c1e60557670647e525b2
Draginalo <tropicaltisa@gmail.com>
2025-04-06 12:44:15 -0400

    Added defensive bomb VFX.

--- Assets/Scripts/VFX/VFXHandler.cs
+++ Assets/Scripts/VFX/VFXHandler.cs
@@ -6,9 +6,32 @@ using UnityEngine.VFX;
 [RequireComponent(typeof(VisualEffect))]
 public class VFXHandler : MonoBehaviour
 {
+    public enum VFXType
+    {
+        OFFENSIVE_BOMB,
+        DEFFENSIVE_BOMB
+    }
+
+    [SerializeField] private VFXType mType;
+
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, GetComponent<VisualEffect>().GetFloat("Lifetime"));
+        switch (mType)
+        {
+            case VFXType.OFFENSIVE_BOMB:
+                Destroy(gameObject, GetComponent<VisualEffect>().GetFloat("Lifetime"));
+                break;
+            case VFXType.DEFFENSIVE_BOMB:
+                StartCoroutine(Co_StupidHelperFunctionToSetAValueYouCantInVFXGraph(GetComponent<VisualEffect>().GetFloat("Lifetime")));
+                break;
+        }
+    }
+
+    private IEnumerator Co_StupidHelperFunctionToSetAValueYouCantInVFXGraph(float lifetime)
+    {
+        yield return new WaitForSeconds(lifetime * 0.5f);
+        GetComponent<VisualEffect>().SetFloat("InnerSpawnCount", 0);
+        Destroy(gameObject, lifetime);
     }
 }
